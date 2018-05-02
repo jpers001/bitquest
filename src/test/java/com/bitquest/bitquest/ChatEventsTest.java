ac29d20a5c7b27fa3d617483ab9099a971831dfe
jpers001 <jpers001@odu.edu>
2018-05-02 03:46:50 +0000

    Adding Unit Testing Files for Future Implementation
    Generic files with generic unit tests for now

---
+++ src/test/java/com/bitquest/bitquest/ChatEventsTest.java
@@ -0,0 +1,14 @@
+package com.bitquest.bitquest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChatEventsTest {
+
+	@Test
+	public void test() {
+		assertEquals(1+1, 2);
+	}
+
+}
---
+++ src/test/java/com/bitquest/bitquest/ServerEventsTest.java
@@ -0,0 +1,14 @@
+package com.bitquest.bitquest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServerEventsTest {
+
+	@Test
+	public void test() {
+		assertEquals(1+1, 2);
+	}
+
+}
